0ceda93dee19789fd67bb5ee30dfc55531e07dfe
Nirbhay Singh <nirbhaymahan100@gmail.com>
2024-10-18 14:50:05 +0530

    Feat: Added error handler middleware with basic error classes

--- src/controllers/problem.controller.js
+++ src/controllers/problem.controller.js
@@ -1,12 +1,16 @@
 const { StatusCodes } = require("http-status-codes");
+const NotImplemented = require("../errors/notimplemented.error");
+const BadRequest = require("../errors/badrequest.error");
 
 function pingProblemController(req, res) {
   return res.json({ message: "Problem controller is up running" });
 }
-function addProblem(req, res) {
-  return res
-    .status(StatusCodes.NOT_IMPLEMENTED)
-    .json({ message: "Not implemented" });
+function addProblem(req, res, next) {
+  try {
+    throw new NotImplemented("addProblem");
+  } catch (err) {
+    next(err);
+  }
 }
 function getProblem(req, res) {
   return res
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const { PORT } = require("./config/server.config");
 const bodyParser = require("body-parser");
 const apiRouter = require("./routes");
+const { errorHandler } = require("./utils/errorHandler");
 
 const app = express();
 
@@ -15,6 +16,9 @@ app.use("/api", apiRouter);
 app.get("/ping", (req, res) => {
   return res.json({ message: "Problem service is alive" });
 });
+
+//last middleware ifany error comes
+app.use(errorHandler);
 
 app.listen(PORT, () => {
   console.log(`Server is running on ${PORT}`);
